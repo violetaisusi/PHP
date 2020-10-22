e444be83fa64817881bfb191f2c5ab1907b450cd
Violeta <violetaisusi@gmail.com>
2020-10-22 10:31:00 +0200

    final version vectors....to learn 1

--- Vectors_PHP_exercises_to_learn_1.php
+++ Vectors_PHP_exercises_to_learn_1.php
@@ -1,5 +1,19 @@
-//Vectors_PHP_exercises_to_learn_1.php
 
 <?php
+//Vectors_PHP_exercises_to_learn_1.php
+echo "I'm in my Vectors_PHP_exercises_to_learn_1.php file";
+
+$weekdays[]="Monday";
+$weekdays[]="Tuesday";
+$weekdays[]="Wednesday";
+$weekdays[]="Thursday";
+$weekdays[]="Friday";
+$weekdays[]="Saturday";
+$weekdays[]="Sunday";
+
+for($i=0 ; $i<count($weekdays) ; $i++)
+{
+    echo "<p>We are accessing to position <b>$i</b> and content <b> $weekdays[$i]</b></p>";
+}
 
 ?>
